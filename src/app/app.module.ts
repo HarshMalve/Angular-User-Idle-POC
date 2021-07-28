70af11d3aca82241940ccc2d24db5eac49bbbb31
Harshavardhan Malve <>
2021-07-28 12:27:31 +0530

    angular-user-idle used in project to check if the user is idle for specific minutes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatInputModule } from '@angular/material/input';
+import {  MatCardModule } from "@angular/material/card";
+
+import { UserIdleModule } from 'angular-user-idle';
+import { TimerComponent } from './components/timer/timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatBadgeModule,
+    // Optionally you can set time for `idle`, `timeout` and `ping` in seconds.
+    // Default values: `idle` is 600 (10 minutes), `timeout` is 300 (5 minutes) 
+    // and `ping` is 120 (2 minutes).
+    UserIdleModule.forRoot({idle: 600, timeout: 300, ping: 120})
   ],
   providers: [],
   bootstrap: [AppComponent]
